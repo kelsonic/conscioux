5b766eca1d6ff564c3b950ced7827e6754a9f613
sandralouise <sandra.adamshallie@gmail.com>
2016-05-20 07:55:23 +0000

    Update review model with validations

--- app/models/cosmetics_review.rb
+++ app/models/cosmetics_review.rb
@@ -4,6 +4,18 @@ class CosmeticsReview < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   validate :image_size
   
+  validates :type_tag, presence: true
+  validates :brand, presence: true
+  validates :product_name, presence: true
+  validates :store, presence: true
+  validates :city, presence: true
+  validates :country, presence: true
+  validates :gender, presence: true
+  validates :cosmetics_review_title, presence: true, length: { in: 4..60 }
+  validates :cosmetics_review_body, presence: true, length: { in: 20..2500 }
+  validates :price, presence: true
+  validates :quantity, presence: true
+  
   def self.search(search)
     where('product_name || brand LIKE ?', "%#{search}%")
   end
