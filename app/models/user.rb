d8ac647e448998c630693d8babd6265ca6c9726b
sanlouise <sandra.adamshallie@gmail.com>
2016-07-28 20:00:55 +0200

    Delete user model in favor of devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,6 @@
 class User < ActiveRecord::Base
-  
-  # Associations
-  has_many :reviews
-  has_many :posts
-  has_one :profile
-
-  # Validations  
-  validate :image_size
-  validates :email, presence: true, uniqueness: {case_sensitive: false}, format: {with: /@/}
-  has_secure_password
-  # TODO: admin validation
-
-  mount_uploader :image, ImageUploader
-  
-  private
-    def image_size
-      if image.size > 1.megabytes
-        errors.add(:image, "Should be less than 1MB")
-      end
-    end
-  
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
