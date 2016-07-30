fa45d1df4032e75db8ee815519006f794fcbba9f
kelsonic <kgadams93@gmail.com>
2016-07-30 16:23:06 +0200

    Delete require_user helper method from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def require_user
-    if !user_signed_in? 
-      flash[:danger] = "You must be logged in to perform this action."
-      redirect_to root_path
-    end
-  end
-  
 end
