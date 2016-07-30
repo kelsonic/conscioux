a5e87a0537e9dabbfb412fd9bfad076d2f844735
kelsonic <kgadams93@gmail.com>
2016-07-30 16:19:40 +0200

    Update image validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,24 +5,14 @@ class Review < ActiveRecord::Base
   belongs_to :product
 
   # Validations
-  validate :image_size
   validates :title, presence: true, length: {maximum: 100}
   validates :body, presence: true, length: {minimum: 50}
   validates :rating, presence: true, numericality: {only_integer: false}
   validates :video, length: {maximum: 500}
-
-  mount_uploader :image, ImageUploader
+  validates :image, length: { maximum: 100 }
   
   def self.search(search)
     where("product_name LIKE ? OR brand LIKE ? OR specific_type_tag LIKE?", "%#{search}%", "%#{search}%", "%#{search}%")
   end
-
-  private
-  
-    def image_size
-      if image.size > 1.megabytes
-        errors.add(:image, "Should be less than 1MB")
-      end
-    end
   
 end
