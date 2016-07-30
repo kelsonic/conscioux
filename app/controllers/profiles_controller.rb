5f2ace078ed41477d9f82461095c1b23e59d1338
sanlouise <sandra.adamshallie@gmail.com>
2016-07-30 20:13:12 +0200

    Remove unused routes and update logic in show, edit, and update actions

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,34 +1,27 @@
 class ProfilesController < ApplicationController  
+
+  before_action :set_profile
   
   
   def show
   end
 
-  def index
-
-  end
-
-  def new
-
-  end
-
-  def create
-
-  end
-
   def edit
-
   end
 
   def update
-
-  end
-
-  def destroy
-
+    if @profile.update(profile_params)
+      redirect_to @profile, notice: 'Profile was updated successfully.'
+    else
+      render :edit
+    end
   end
 
   private
+
+    def set_profile
+      @profile = Profile.find(params[:id])
+    end
 
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :country, :city, :country, :bio, :image)
