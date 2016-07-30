79ee1ad0a2a00cd99cd0e2f56634673689e0cce0
kelsonic <kgadams93@gmail.com>
2016-07-30 16:20:14 +0200

    Update image validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,22 +5,12 @@ class Product < ActiveRecord::Base
   has_many :reviews
 
   # Validations
-  validate :image_size
   validates :name, presence: true, length: { maximum: 150 }
   validates :brand, presence: true, length: { maximum: 150 }
   validates :gender, presence: true
   # TODO: validates :website
   validates :price, format: { with: /[^A-Za-z]/ }
   validates :quantity, length: { maximum: 50 }
-
-  mount_uploader :image, ImageUploader
-
-  private
-
-    def image_size
-      if image.size > 1.megabytes
-        errors.add(:image, "Should be less than 1MB")
-      end
-    end
+  validates :image, length: { maximum: 100 }
 
 end
