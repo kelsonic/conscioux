55d50648d4429c12f175b33ea38a548b11372796
kelsonic <kgadams93@gmail.com>
2016-07-30 16:22:15 +0200

    Add set_product and authenticate_user! before actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,34 +1,47 @@
 class ProductsController < ApplicationController
   
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  
   def show
-
-  end
-
-  def index
-
   end
 
   def new
-
-  end
-
-  def create
-
+    @product = Product.new
   end
 
   def edit
+  end
 
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to @product, notice: 'Product was created successfully.'
+    else
+      render :new
+    end
   end
 
   def update
-
+    if @product.update(product_params)
+      redirect_to @product, notice: 'Product was updated successfully.'
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @product.destroy
+    redirect_to category_products_path, notice: 'Product was destroyed successfully.'
   end
 
   private
+
+    def set_product
+      p params
+      @product = Product.find(params[:id])
+    end
 
     def product_params
       params.require(:product).permit(:name, :store, :city, :country, :gender, :website, :image, :price, :quantity)
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,4 @@
-class ProfilesController < ApplicationController
-  
+class ProfilesController < ApplicationController  
   
   
   def show
