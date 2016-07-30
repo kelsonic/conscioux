85f272c52fbfb21458141044d606fdb140e899b7
kelsonic <kgadams93@gmail.com>
2016-07-30 16:20:29 +0200

    Update image validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   # Validations
-  validate :image_size
   validates :topic, presence: true, length: {maximum: 150}
   validates :city, presence: true, format: {with: /[^0-9!@#$%^&*()]+/, message: 'No special characters'}, length: {maximum: 200}
   validates :country, presence: true, format: {with: /[^0-9!@#$%^&*()]+/, message: 'No special characters'}, length: {maximum: 200}
@@ -13,15 +12,6 @@ class Post < ActiveRecord::Base
   validates :takeaways, length: {maximum: 500}
   validates :interviewed_people, length: {maximum: 1000}
   # TODO: video validations
-
-  mount_uploader :image, ImageUploader
-
-  private
-
-    def image_size
-      if image.size > 1.megabytes
-        errors.add(:image, "Should be less than 1MB")
-      end
-    end
+  validates :image, length: { maximum: 100 }
   
 end
