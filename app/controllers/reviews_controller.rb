52ab730c89e27d722e19bd0aa70fac3b3483fb9b
sanlouise <sandra.adamshallie@gmail.com>
2016-07-31 17:20:32 +0200

    Fix merge conflict

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,10 @@
 class ReviewsController < ApplicationController
+  before_action :set_category_and_product, only: [:new, :create, :edit, :update]
   before_action :set_review, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def show
+    @featured_reviews = Review.all.shuffle.take(5)
   end
 
   def index
@@ -21,7 +23,7 @@ class ReviewsController < ApplicationController
     @review.user_id = current_user.id
 
     if @review.save
-      redirect_to @review, notice: 'Review was created successfully.'
+      redirect_to category_product_review_path(@category, @product, @review), notice: 'Review was created successfully.'
     else
       render :new
     end
@@ -29,7 +31,7 @@ class ReviewsController < ApplicationController
 
   def update
     if @review.update(review_params)
-      redirect_to @review, notice: 'Review was updated successfully.'
+      redirect_to category_product_review_path(@category, @product, @review), notice: 'Review was updated successfully.'
     else
       render :new
     end
@@ -37,10 +39,15 @@ class ReviewsController < ApplicationController
 
   def destroy
     @review.destroy
-    redirect_to reviews_path, notice: 'Review was destroyed successfully.'
+    redirect_to category_product_path(@review.product.category, @review.product), notice: 'Review was destroyed successfully.'
   end
 
   private
+
+    def set_category_and_product
+      @category = Category.find(params[:category_id])
+      @product = Product.find(params[:product_id])
+    end
 
     def set_review
       @review = Review.find(params[:id])
