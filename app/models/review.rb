17a4a39d4ae30f0cb0acb9bb08a7f1ed04800de2
kelsonic <kgadams93@gmail.com>
2016-07-31 22:55:33 +0200

    Update validations and delete instance method

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,14 +5,10 @@ class Review < ActiveRecord::Base
   belongs_to :product
 
   # Validations
-  validates :title, presence: true, length: {maximum: 100}
-  validates :body, presence: true, length: {minimum: 50}
-  validates :rating, presence: true, numericality: {only_integer: true}
-  validates :video, length: {maximum: 500}
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :body, presence: true, length: { maximum: 5000 }
+  validates :rating, presence: true, numericality: { only_integer: true }
+  validates :video, length: { maximum: 500 }
   validates :image, length: { maximum: 100 }
-  
-  def self.search(search)
-    where("product_name LIKE ? OR brand LIKE ? OR specific_type_tag LIKE?", "%#{search}%", "%#{search}%", "%#{search}%")
-  end
   
 end
